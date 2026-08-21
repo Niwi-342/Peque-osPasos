241260aeb76b17e706b6de0b91bfe7f4233ad0ab
Claudia Soledad Barboza <estudiantecolegio373@gmail.com>
2026-08-21 12:46:42 -0300

    Add files via upload

---
+++ Proyectos de Práctica/holamundo.js/tipo_de_datos.js
@@ -0,0 +1,37 @@
+//Este es una práctica de parte de Freecodecamp
+
+// Los tipos de datos
+
+//variables: Son contenedores que almacenan datos
+const caja_de_basura = "Ropa vieja, cajas de papel, etc." 
+//quiero aclarar que estan tambien las constantes, que son definitivas
+//y no se pueden cambiar
+
+
+//en fin, el valor que almacena es un string, en donde se almacena
+//cualquier cosa con comillas
+
+let numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10); 
+
+//este tipo de dato es son numeros es decir; number
+
+let numeros_decimales = (1.2, 4.5, 4.06);
+
+//almacena decimales numeros decimales 
+
+let calificacion = ['Juan', 12, true, 'Perez']; 
+
+//esta es una lista (arrays)
+
+let comentario = true;
+let comentario_2 = false;
+
+//estos son booleanos, en ellos se decide uno es:
+// 'verdad' o 'falso' 
+
+
+ //Conforme aprenda mas, habra mas tipo de datos
+
+ 
+
+
